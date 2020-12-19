538cdf2ec18cbdae18d4e6f74062799f272245eb
rohannegi <rohangovindnegi@yahoo.com>
2020-12-19 13:08:10 -0500

    some prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,58 @@
 // Assignment code here
+var length;
+var numeric;
+var special;
+var uppercase;
+var lowercase;
+var options;
+
+
+
+
+function generatePassword() {
+
+  length = prompt("how much length of characters do you want for your password? Choose between 8 and 128");
+
+  if (!length) {
+    alert("A value is required to proceed");
+  } else if (length < 8 || length > 128) {
+    length = prompt(" A number has to be chosen only between between 8 and 128")
+  
+  } else {
+
+    numeric = confirm("Do you want numbers in your password? click 'ok' for yes and 'cancel' for no");
+    special = confirm("Do you want special characters in your password? click 'ok' for yes and 'cancel' for no");
+    uppercase = confirm("Do you want uppercase characters in your password? click 'ok' for yes and 'cancel' for no");
+    lowercase = confirm("Do you want lowercase characters in your password? click 'ok' for yes and 'cancel' for no");
+};
+
+if (!special && !numeric && !uppercase && !lowercase) {
+    alert("you haven't chosen any criteria. Atleast one criteria must be chosen to continue.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
 
 
 // Get references to the #generate element
